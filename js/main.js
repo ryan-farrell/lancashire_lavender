ec3475ad150ecce0cb39f52b0bd153b2e919976c
ryan-farrell <ryan.farrell44@gmail.com>
2019-06-30 00:02:51 +0100

    Added hero video on landing page. Tweaked some CSS and added section for Yozgan to insert his form code.

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let aboutNav = document.getElementById('about')
+
+// aboutNav.addEventListener('click', () => {
+//     console.log('Working')
+
+//     document.getElementsById('aboutHere').scrollIntoView({behavior: "smooth"});
+
+// });
+
+
+
+function about () {
+   console.log('Working')
+   let ab = document.getElementsById('aboutHere')
+   ab.scrollIntoView({behavior: "smooth"});
+};
+
+
+
+// function about () {
+
+//     let aboutHere = document.getElementById('#aboutHere')
+//     aboutHere.scrollIntoView({
+//     behavior: 'smooth'})
+
+//     console.log('here2')
+
+
+// }
+
+
+// let element = document.getElementById('subheader');
+// let [btn] = document.getElementsByTagName('button');
+// btn.addEventListener('click', ()=>{
+//     element.scrollIntoView({behavior: "smooth"});
+// })
